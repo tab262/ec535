20ec048483cdf3cf13b03b270ba9de586d2840f5
mikedinicola <mikedinicola@users.noreply.github.com>
2014-12-10 20:20:35 -0500

    Update slidersgroup.h
    Cleanup and comment

--- gumstix_code/controller/slidersgroup.h
+++ gumstix_code/controller/slidersgroup.h
@@ -14,7 +14,6 @@ class SlidersGroup : public QGroupBox
 public:
     SlidersGroup(Qt::Orientation orientation, const QString &title,
                  QWidget *parent = 0);
-    int tag;
 
 signals:
     void valueChanged(int value);
@@ -23,8 +22,6 @@ public slots:
     void setValue(int value);
     void setMinimum(int value);
     void setMaximum(int value);
-    void invertAppearance(bool invert);
-    void invertKeyBindings(bool invert);
 
 private:
     QSlider *slider;
